6794e116f4a969af3b6cf19c1aa49d88fe5dd854
Dylan <dylanrios211@gmail.com>
2023-11-15 21:50:39 -0500

    Adelanto Logica Sing_in y Sing_up

--- JS/Proyecto.js
+++ JS/Proyecto.js
@@ -1,4 +1,38 @@
+function Register() {
+    const nombre = document.getElementById("Usuario").value;
+    const apellido = document.getElementById("Contraseña").value;
+    const sexo = document.getElementById("sexo").value;
+
+    const usuario = {
+        nombre,
+        sexo,
+        contrasena
+    };
+    localStorage.setItem(correo, JSON.stringify(usuario));
+
+    alert("Usuario creado correctamente. Por favor, inicie sesión.");
+}
+
 function Login() {
-    let 
-    
+    const Usuario_login = document.getElementById("Usuario_login").value;
+    const Contraseña_login = document.getElementById("Contraseña_login").value;
+
+    const usuarioGuardado = localStorage.getItem(Usuario_login);
+
+    if (usuarioGuardado) {
+        const usuario = JSON.parse(usuarioGuardado);
+
+        if (usuario.contraseña === Contraseña_login) {
+            document.getElementById("mensajeError").innerText = '';
+
+            location.href="../Principal.html";
+            document.getElementById("nombreUsuario").innerText = usuario.nombre;
+
+            document.getElementById("app").style.display = 'none';
+        } else {
+            document.getElementById("mensajeError").innerText = "Contraseña incorrecta";
+        }
+    } else {
+        document.getElementById("mensajeError").innerText = "Usuario no registrado";
+    }
 }
